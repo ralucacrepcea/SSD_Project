09c9ae75ae6b4ecc63a69c68d3f99020b00f6bf8
bianca081002 <b.maier15@icloud.com>
2024-12-13 21:44:06 +0200

    modified App

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,6 @@ import React from 'react'
 export default function App() {
 
   return (
-    <h1 className = 'text-red-500'>App Real estate application</h1>
+    <h1 className = 'text-red-500'>App Real estate application.</h1>
   )
 }
